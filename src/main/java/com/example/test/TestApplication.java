0994bf28dad5c80c6b56e3a0be0a67325b6c40fe
Gawsan <67488752+Gawsan@users.noreply.github.com>
2024-04-23 18:52:17 +0530

    sonar cloud test

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApplication {
 
+    //sonar cloud test
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
